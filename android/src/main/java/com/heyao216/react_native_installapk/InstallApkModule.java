8423a21c5d534c82dba73c1698f524011ab96ef1
fenglu09 <fenglu09@gmail.com>
2019-09-15 12:50:53 +0800

    support库更新成androidx

--- android/src/main/java/com/heyao216/react_native_installapk/InstallApkModule.java
+++ android/src/main/java/com/heyao216/react_native_installapk/InstallApkModule.java
@@ -5,7 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.v4.content.FileProvider;
+
+import androidx.core.content.FileProvider;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
